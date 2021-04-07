552164ad27be25c42632c0079e9e443b619b9d5b
Loreto Delgado III <loretodelgadoiii@loretos-mbp.home>
2021-04-07 18:01:43 -0400

    added ability to check if a product qualifies for free shipping

--- TypeMart/index.js
+++ TypeMart/index.js
@@ -1,8 +1,18 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 const products_1 = require("./products");
-let productName = "fanny pack";
+const productName = "fanny pack";
+let shipping = 5;
+let taxPercent;
+let taxTotal;
+let total;
+let shippingAddress;
 const product = products_1.default.filter(product => product.name === productName)[0];
 console.log(product);
-if (product.preOrder)
+if (product.preOrder === "true")
     console.log("Your order will be shipped soon!");
+if (Number(product.price) >= 25) {
+    shipping = 0;
+    console.log("This product qualifies for free shipping!");
+}
+;
--- TypeMart/index.ts
+++ TypeMart/index.ts
@@ -1,9 +1,19 @@
 import products from "./products"
 
-let productName: string = "fanny pack";
+const productName: string = "fanny pack";
+let shipping: number = 5;
+let taxPercent: number;
+let taxTotal: number;
+let total: number;
+let shippingAddress: string;
 
 const product = products.filter(product => product.name === productName)[0];
 
 console.log(product);
 
-if(product.preOrder) console.log("Your order will be shipped soon!")
+if(product.preOrder === "true") console.log("Your order will be shipped soon!")
+
+if(Number(product.price) >= 25) {
+    shipping = 0
+    console.log("This product qualifies for free shipping!")
+};
