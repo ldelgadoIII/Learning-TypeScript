ccef52107495d988033e2df2ef15e1e8d9ff6416
Loreto Delgado III <loretodelgadoiii@loretos-mbp.home>
2021-04-07 17:51:04 -0400

    added condition that checks if the product is on pre-order

--- TypeMart/index.js
+++ TypeMart/index.js
@@ -4,3 +4,5 @@ const products_1 = require("./products");
 let productName = "fanny pack";
 const product = products_1.default.filter(product => product.name === productName)[0];
 console.log(product);
+if (product.preOrder)
+    console.log("Your order will be shipped soon!");
--- TypeMart/index.ts
+++ TypeMart/index.ts
@@ -4,4 +4,6 @@ let productName: string = "fanny pack";
 
 const product = products.filter(product => product.name === productName)[0];
 
-console.log(product)
+console.log(product);
+
+if(product.preOrder) console.log("Your order will be shipped soon!")
