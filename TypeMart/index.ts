c1276929d77ad07232f3c27d1c9ae921d3d6b1ff
Loreto Delgado III <loretodelgadoiii@loretos-mbp.home>
2021-04-13 13:56:46 -0400

    added tax total and total calculations. printed out all info about the product and prices

--- TypeMart/index.ts
+++ TypeMart/index.ts
@@ -9,16 +9,23 @@ let shippingAddress: string = "123 W. Street St. New York, New York";
 
 const product = products.filter(product => product.name === productName)[0];
 
-console.log(product);
-
 if(product.preOrder === "true") console.log("Your order will be shipped soon!")
 
 if(Number(product.price) >= 25) {
     shipping = 0
-    console.log("This product qualifies for free shipping!")
 };
 
 if(shippingAddress.match("New York")) {
     taxPercent = .1;
-    console.log("NYC Tax included.")
 }
+
+taxTotal = Number(product.price) * taxPercent;
+
+total = Number(product.price) + taxTotal + shipping;
+
+console.log("Product Name: ", product.name)
+console.log("Shipping address: ", shippingAddress)
+console.log("Price of the product: ", Number(product.price))
+console.log("Tax total: ", taxTotal)
+console.log("Shipping: ", shipping)
+console.log("Total amount: ", total)
