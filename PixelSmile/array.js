055a70852ba73c69abd793bc51fcded51ca84409
Loreto Delgado III <loretodelgadoiii@loretos-mbp.home>
2021-04-16 15:00:02 -0400

    added a type check to an array

---
+++ PixelSmile/array.js
@@ -0,0 +1,10 @@
+let customersArray = ['Custy Stomer', 'C. Oostomar', 'C.U.S. Tomer', 3432434, 'Custo Mer', 'Custopher Ustomer', 3432435, 'Kasti Yastimeur'];
+//Write Your Code here:
+function checkCustomersArray() {
+    customersArray.forEach(el => {
+        if (typeof el !== "string") {
+            console.log(`Type error: ${el} should be a string!`);
+        }
+    });
+}
+checkCustomersArray();
