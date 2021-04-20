b144c28de152455ea379d41d77901f955b7bc370
Loreto Delgado III <loretodelgadoiii@loretos-mbp.home>
2021-04-20 15:18:30 -0400

    fixed error in type alias Restaurant

---
+++ RestaurantOrders/index.js
@@ -0,0 +1,8 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+/// Add your getMaxPrice() function below:
+/// Add your getOrders() function below:
+/// Add your printOrders() function below:
+/// Main
+// const elligibleOrders = getOrders(PriceBracket.Low, orders);
+// printOrders(restaurants, elligibleOrders);
