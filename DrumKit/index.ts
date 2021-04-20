9607f0fcb84da10a810f3600bd91dd7954029506
Loreto Delgado III <loretodelgadoiii@loretos-mbp.home>
2021-04-20 14:38:16 -0400

    added type to e param

--- DrumKit/index.ts
+++ DrumKit/index.ts
@@ -1,6 +1,6 @@
 const keys = document.querySelectorAll(".key");
 
-function playsound(e):void {
+function playsound(e:any):void {
     const audio:any = document.querySelector(`audio[data-key="${e.keyCode}"]`)
     const key:any = document.querySelector(`.key[data-key="${e.keyCode}"]`)
     audio.currentTime = 0;
