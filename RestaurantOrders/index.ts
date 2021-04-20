5922bd9997a1b2249eefd5492dc6ece316d0457b
Loreto Delgado III <loretodelgadoiii@loretos-mbp.home>
2021-04-20 15:29:44 -0400

    added a switch case to maxprice function

--- RestaurantOrders/index.ts
+++ RestaurantOrders/index.ts
@@ -2,6 +2,21 @@ import { restaurants, Restaurant } from "./restaurants";
 import { orders, Order, PriceBracket } from "./orders";
 
 /// Add your getMaxPrice() function below:
+function getMaxPrice(price: PriceBracket){
+    switch (price) {
+        case PriceBracket.Low:
+            return 10.0;
+            break;
+        case PriceBracket.Medium:
+            return 20.0;
+            break;
+        case PriceBracket.High:
+            return 30.0;
+            break;
+        default:
+            return "None";
+    }
+}
 
 /// Add your getOrders() function below:
 
