d6bada67008cb8a8a549a78e08b16ad9013233e8
Loreto Delgado III <loretodelgadoiii@Loretos-MacBook-Pro.local>
2021-05-17 16:47:55 -0400

    added Car class and implemented the AutonomousCar type to it

--- Self Driving Car/index.ts
+++ Self Driving Car/index.ts
@@ -1,1 +1,13 @@
 import { getObstacleEvents } from './computer-vision';
+
+// TYPES ===========================
+interface AutonomousCar {
+    isRunning?: boolean
+}
+
+// CLASSES =========================
+class Car implements AutonomousCar {
+
+}
+
+// EXECUTION =======================
